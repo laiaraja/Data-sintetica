32b200413ad65a107812e8277f1d392f16b9cff2
laiaraja <laiaraja.ub@gmail.com>
2026-02-27 09:18:28 +0100

    first commit

---
+++ data_1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+# 1. Cargar el archivo
+df = pd.read_csv('MOCK_DATA.csv')
+
+# 3. Añadir Outliers (Valores fuera de rango) manuales
+# Cambiamos la edad de los registros 12 al 19 a valores absurdos
+df.loc[12:19, 'age'] = 999 
+
+# Cambiamos el BMI de la línea 50 a un valor negativo
+df.loc[50, 'BMI'] = -10.5
+
+# 4. Guardar el archivo "sucio"
+df.to_csv('MOCK_DATA_mod.csv', index=False)
